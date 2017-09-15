ea4820d3badd81fbc9ac87829a9d0185e55ecbc1
harakan <will@createcafe.ca>
2017-09-15 15:09:01 -0600

    Removed a bunch of deadcode used for registering to particle cloud

--- C/application.cpp
+++ C/application.cpp
@@ -29,12 +29,6 @@ SerialDebugOutput debugOutput(115200, ALL_LEVEL);
 STARTUP(cellular_credentials_set("pda.bell.ca", "", "", NULL)); // BELL
 //STARTUP(cellular_credentials_set("broadband", "", "", NULL)); // AT&T
 
-/* Function prototypes -------------------------------------------------------*/
-int tinkerDigitalRead(String pin);
-int tinkerDigitalWrite(String command);
-int tinkerAnalogRead(String pin);
-int tinkerAnalogWrite(String command);
-
 //SYSTEM_MODE(AUTOMATIC);
 //SYSTEM_MODE(SEMI_AUTOMATIC);
 SYSTEM_MODE(MANUAL);
@@ -43,7 +37,6 @@ uint32_t lastUpdate = 0;
 bool D7state = false;
 
 typedef struct { char* buf; char* num; } CMGRparam;
-//static int _cbCUSD(int type, const char* buf, int len, char* resp);
 typedef struct { int* ix; int num; } CMGLparam;
 int _cbCMGL(int type, const char* buf, int len, CMGLparam* param);
 int _cbCMGR(int type, const char* buf, int len, CMGRparam* param);
@@ -80,40 +73,6 @@ int smsList(const char* stat /*= "ALL"*/, int* ix /*=NULL*/, int num /*= 0*/) {
     return ret;
 }
 
-/*
-bool smsSend(const char* num, const char* buf)
-{
-    //REF:
-    //https://community.particle.io/t/sending-sms-from-particle-electron-using-at-commands/21020
-    int resp;
-    bool ok = false;
-    char tmp[64];
-
-    resp = Cellular.command(1000, "AT+CMGF=1\r\n"); //Start SMS
-    Serial.printf("got (%d) in send 1nd command \r\n",resp);
-
-    // numberformat 145=ISDN/ITU (uses +13061234567) 
-    // vs numberformat 129 *seems* to be all else (ie: 3061234567)
-    //~english - (<TIMEOUT>,<ATCMD>="<PHONENUM>",<NUM_FORMAT>)
-
-    //sprintf(pnumAtcmd, "AT+CMGS
-    resp = Cellular.command(1000, "AT+CMGS=\"%s\",145\r\n", num);
-    Serial.printf("got (%d) in send 2nd command \r\n",resp);
-
-    // Close with <MESSAGE>CTRL+Z (or 0x1A)
-    // Note: no need for /r/n shiz in sms messages
-    resp = Cellular.command(60*1000, "%s", buf);
-    Serial.printf("got (%d) in send 3rd command \r\n",resp);
-
-    resp = Cellular.command(60*1000, "%c", 0X1A);
-    Serial.printf("got (%d) in send 4th command \r\n",resp);
-    ok = (resp == RESP_OK); 
-    return ok;
-}
-*/
-
-
-//Commenting out the default and remaking my own
 bool smsSend(const char* num, const char* buf)
 {
     bool ok = false;
@@ -159,35 +118,16 @@ bool smsDelete(int ix)
 int _cbCMGR(int type, const char* buf, int len, CMGRparam* param)
 {
 
-    //Serial.printf("\r\nfirst cbCMGR: %s\r\n", buf);
-    //Serial.printf("length: %d\r\n",len);
-    //Serial.printf("Type: %d\r\n",type);
     
     if (type == TYPE_PLUS) {
         if (sscanf(buf, "\r\n+CMGR: \"%*[^\"]\",\"%[^\"]", param->num) == 1) {
-            //buf="";//if it's the number, store it and clear the buffer
+            // extract number - if number is valid do ______?
         }
-    }
+    } //else it must be a text message
     else if ((type == TYPE_UNKNOWN) && (len>2)) {
         memcpy(param->buf, buf, len);
         param->buf[len] = '\0';
-        //Serial.printf("\r\nNEW: after param->buf=%s\r\n",param->buf);
     } 
-    //if (param) {
-    if (0) {
-        if (type == TYPE_PLUS) {
-            if (sscanf(buf, "\r\n+CMGR: \"%*[^\"]\",\"%[^\"]", param->num) == 1) {
-            }
-        Serial.printf("\r\ncbCMGR: type=%d buf=%s len=%d DONE\r\n",type,buf,len);
-        //Serial.printf("cbCMGR: type=%d buf[len-2]=%x buf[len-1]=%x  DONE\r\n",type,buf[len-2],buf[len-1]);
-        //WILL: Changed this to match texts recieved from beths phone
-        //Serial.printf("\r\ncbCMGR: param->buf=%s\r\n",param->buf);
-        }if ((type == TYPE_PLUS) && (buf[len-2] == '\r') && (buf[len-1] == '\n')) {
-            memcpy(param->buf, buf, len-2);
-            param->buf[len-2] = '\0';
-            Serial.printf("\r\ncbCMGR: after param->buf=%s\r\n",param->buf);
-        }
-    }
     return WAIT;
 }
 
@@ -208,13 +148,13 @@ void checkUnreadSMS() {
     int n = smsList("REC UNREAD", ix, 8);
     bool sendStatus=0;
     if (n > 8) n = 8;
-    while (n-- > 0)
+    while (n-- > 0) //parse all messages in the queue
     {
         char num[32];
         Serial.printf("Unread SMS at index %d\r\n", ix[n]);
         if (smsRead(ix[n], num, buf, sizeof(buf))) {
             Serial.printf("Got SMS from \"%s\" with text \"%s\"\r\n", num, buf);
-            // provide a generic reply
+            // provide a generic default reply
             const char* reply = "Hi, Wills Sim card is in a test device this is an automated reply as your text is now lost";
 
             // If message contains D7 or d7
@@ -280,15 +220,8 @@ void setup()
     }
     resp = Cellular.command(1000, "AT+CMGF=1\r\n");
     Serial.printf("Cellular connected, set mode to text: %d (-2==OK)\r\n",resp);
+    digitalWrite(D7, LOW);
 
-    //Setup the Tinker application here
-
-    //Register all the Tinker functions
-    //Particle.function("digitalread", tinkerDigitalRead);
-    //Particle.function("digitalwrite", tinkerDigitalWrite);
-
-    //Particle.function("analogread", tinkerAnalogRead);
-    //Particle.function("analogwrite", tinkerAnalogWrite);
 }
 
 /* This function loops forever --------------------------------------------*/
@@ -340,204 +273,6 @@ void loop()
         }
         while (Serial.available()) Serial.read(); // Flush the input buffer
     }
-}
-
-/*******************************************************************************
- * Function Name  : tinkerDigitalRead
- * Description    : Reads the digital value of a given pin
- * Input          : Pin
- * Output         : None.
- * Return         : Value of the pin (0 or 1) in INT type
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerDigitalRead(String pin)
-{
-    //convert ascii to integer
-    int pinNumber = pin.charAt(1) - '0';
-    //Sanity check to see if the pin numbers are within limits
-    if (pinNumber < 0 || pinNumber > 7) return -1;
-
-    if(pin.startsWith("D"))
-    {
-        pinMode(pinNumber, INPUT_PULLDOWN);
-        return digitalRead(pinNumber);
-    }
-    else if (pin.startsWith("A"))
-    {
-        pinMode(pinNumber+10, INPUT_PULLDOWN);
-        return digitalRead(pinNumber+10);
-    }
-#if Wiring_Cellular
-    else if (pin.startsWith("B"))
-    {
-        if (pinNumber > 5) return -3;
-        pinMode(pinNumber+24, INPUT_PULLDOWN);
-        return digitalRead(pinNumber+24);
-    }
-    else if (pin.startsWith("C"))
-    {
-        if (pinNumber > 5) return -4;
-        pinMode(pinNumber+30, INPUT_PULLDOWN);
-        return digitalRead(pinNumber+30);
-    }
-#endif
-    return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerDigitalWrite
- * Description    : Sets the specified pin HIGH or LOW
- * Input          : Pin and value
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerDigitalWrite(String command)
-{
-    bool value = 0;
-    //convert ascii to integer
-    int pinNumber = command.charAt(1) - '0';
-    //Sanity check to see if the pin numbers are within limits
-    if (pinNumber < 0 || pinNumber > 7) return -1;
-
-    if(command.substring(3,7) == "HIGH") value = 1;
-    else if(command.substring(3,6) == "LOW") value = 0;
-    else return -2;
-
-    if(command.startsWith("D"))
-    {
-        pinMode(pinNumber, OUTPUT);
-        digitalWrite(pinNumber, value);
-        return 1;
-    }
-    else if(command.startsWith("A"))
-    {
-        pinMode(pinNumber+10, OUTPUT);
-        digitalWrite(pinNumber+10, value);
-        return 1;
-    }
-#if Wiring_Cellular
-    else if(command.startsWith("B"))
-    {
-        if (pinNumber > 5) return -4;
-        pinMode(pinNumber+24, OUTPUT);
-        digitalWrite(pinNumber+24, value);
-        return 1;
-    }
-    else if(command.startsWith("C"))
-    {
-        if (pinNumber > 5) return -5;
-        pinMode(pinNumber+30, OUTPUT);
-        digitalWrite(pinNumber+30, value);
-        return 1;
-    }
-#endif
-    else return -3;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogRead
- * Description    : Reads the analog value of a pin
- * Input          : Pin
- * Output         : None.
- * Return         : Returns the analog value in INT type (0 to 4095)
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerAnalogRead(String pin)
-{
-    //convert ascii to integer
-    int pinNumber = pin.charAt(1) - '0';
-    //Sanity check to see if the pin numbers are within limits
-    if (pinNumber < 0 || pinNumber > 7) return -1;
-
-    if(pin.startsWith("D"))
-    {
-        return -3;
-    }
-    else if (pin.startsWith("A"))
-    {
-        return analogRead(pinNumber+10);
-    }
-#if Wiring_Cellular
-    else if (pin.startsWith("B"))
-    {
-        if (pinNumber < 2 || pinNumber > 5) return -3;
-        return analogRead(pinNumber+24);
-    }
-#endif
-    return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogWrite
- * Description    : Writes an analog value (PWM) to the specified pin
- * Input          : Pin and Value (0 to 255)
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerAnalogWrite(String command)
-{
-    String value = command.substring(3);
-
-    if(command.substring(0,2) == "TX")
-    {
-        pinMode(TX, OUTPUT);
-        analogWrite(TX, value.toInt());
-        return 1;
-    }
-    else if(command.substring(0,2) == "RX")
-    {
-        pinMode(RX, OUTPUT);
-        analogWrite(RX, value.toInt());
-        return 1;
-    }
-
-    //convert ascii to integer
-    int pinNumber = command.charAt(1) - '0';
-    //Sanity check to see if the pin numbers are within limits
-
-    if (pinNumber < 0 || pinNumber > 7) return -1;
-
-    if(command.startsWith("D"))
-    {
-        pinMode(pinNumber, OUTPUT);
-        analogWrite(pinNumber, value.toInt());
-        return 1;
-    }
-    else if(command.startsWith("A"))
-    {
-        pinMode(pinNumber+10, OUTPUT);
-        analogWrite(pinNumber+10, value.toInt());
-        return 1;
-    }
-    else if(command.substring(0,2) == "TX")
-    {
-        pinMode(TX, OUTPUT);
-        analogWrite(TX, value.toInt());
-        return 1;
-    }
-    else if(command.substring(0,2) == "RX")
-    {
-        pinMode(RX, OUTPUT);
-        analogWrite(RX, value.toInt());
-        return 1;
-    }
-#if Wiring_Cellular
-    else if (command.startsWith("B"))
-    {
-        if (pinNumber > 3) return -3;
-        pinMode(pinNumber+24, OUTPUT);
-        analogWrite(pinNumber+24, value.toInt());
-        return 1;
-    }
-    else if (command.startsWith("C"))
-    {
-        if (pinNumber < 4 || pinNumber > 5) return -4;
-        pinMode(pinNumber+30, OUTPUT);
-        analogWrite(pinNumber+30, value.toInt());
-        return 1;
-    }
-#endif
-    else return -2;
 }
 
 void processATcommand() {
@@ -608,3 +343,8 @@ void showHelp() {
                    "\r\n[a] send an AT command"
                    "\r\n[h] show this help menu\r\n");
 }
+
+/**********************************************************/
+// CellLocate Functions
+/**********************************************************/
+
